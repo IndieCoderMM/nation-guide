9929b41507336148c773d400d3a86585c6b72c6a
heinthant <hthant00chk@gmail.com>
2023-02-16 15:22:03 +0630

    Fix eslint errors :(

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CountriesList from './components/CountriesList';
 import CountryDetail from './components/CountryDetail';
