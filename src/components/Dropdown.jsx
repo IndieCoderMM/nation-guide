4ac421e3bb516c3658198b96edcc2ce7c86b9452
IndieCoderMM <hthant00chk@gmail.com>
2023-09-19 00:12:31 +0630

    feat: Customize dropdown menu

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { FaSort } from 'react-icons/fa';
 import styles from '../styles/Dropdown.module.css';
+import { sortingOptions } from '../lib/constants';
 
 const Dropdown = ({ options, defaultValue, onChange }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,43 +10,73 @@ const Dropdown = ({ options, defaultValue, onChange }) => {
 
   const toggleDropdown = () => setIsOpen(!isOpen);
 
-  const handleOptionClick = (value) => {
-    setSelectedOption(value);
-    onChange(value);
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    onChange(option);
     setIsOpen(false);
   };
+
+  useEffect(() => {
+    const handleEscape = (e) => {
+      if (e.key === 'Escape') {
+        setIsOpen(false);
+      }
+    };
+
+    const handleOutsideClick = (e) => {
+      if (!e.target.closest(`.${styles.dropdown}`)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('keydown', handleEscape);
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   return (
     <div className={styles.dropdown}>
       <button
+        type="button"
         className={styles.trigger}
         onClick={toggleDropdown}
         aria-haspopup="listbox"
         aria-expanded={isOpen}
       >
-        {selectedOption}
+        {selectedOption.label}
+        <FaSort />
       </button>
-      {isOpen && (
-        <div className={styles.options} role="listbox">
-          {options.map((option) => (
-            <div
-              key={option}
-              className={styles.option}
-              onClick={() => handleOptionClick(option)}
-              role="option"
-            >
-              {option}
-            </div>
-          ))}
-        </div>
-      )}
+
+      <div
+        className={`${styles.options} ${isOpen ? styles.open : ''}`}
+        role="listbox"
+      >
+        {options.map((option) => (
+          <button
+            key={option.value}
+            type="button"
+            className={styles.item}
+            onClick={() => handleOptionClick(option)}
+            role="option"
+            aria-selected={option === selectedOption}
+          >
+            {option.label}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
 
+const Option = PropTypes.oneOf(sortingOptions);
+
 Dropdown.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  defaultValue: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(Option).isRequired,
+  defaultValue: Option.isRequired,
   onChange: PropTypes.func.isRequired,
 };
 
--- src/components/SortingBox.jsx
+++ src/components/SortingBox.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Dropdown from './Dropdown';
 import styles from '../styles/SortingBox.module.css';
+import { sortingOptions } from '../lib/constants';
 
 const SortingBox = ({ sorter, setSorter }) => (
   <div className={styles.container}>
     <Dropdown
-      options={[
-        { value: 'area-d', label: 'Sort by Area [D]' },
-        { value: 'area-a', label: 'Sort by Area [A]' },
-        { value: 'name-d', label: 'Sort by Name [D]' },
-        { value: 'name-a', label: 'Sort by Name [A]' },
-      ]}
-      value={sorter}
+      options={sortingOptions}
+      defaultValue={sorter}
       onChange={setSorter}
     />
   </div>
 );
 
 SortingBox.propTypes = {
-  sorter: PropTypes.string.isRequired,
+  sorter: PropTypes.oneOf(sortingOptions).isRequired,
   setSorter: PropTypes.func.isRequired,
 };
 
